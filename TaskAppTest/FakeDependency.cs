998e246661cb46f1803b92e9e74416df9471d68e
Shameera Kalubowila <shammakalubo@gmail.com>
2022-05-05 10:29:40 -0400

    Adding NUnit

---
+++ TaskAppTest/FakeDependency.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TaskAppGEICO.Models;
+using TaskAppGEICO.Services;
+
+namespace TaskAppTest
+{
+    public class FakeDependency : ITaskServices
+    {
+        //     public void DoSomething(string value)
+        //{
+        //    InputValue = value;
+        //    ExecutionCount++;
+        //}
+
+        //Task AddTask(TaskTable task, ReturnMessage message);
+        //Task UpdateTask(TaskTable request, ReturnMessage erm);
+        //Task<List<TaskTable>> GetTasks(ReturnMessage erm);
+
+
+        public async Task AddTask(TaskTable task, ReturnMessage message)
+        {
+            taskTable = task;
+            ExecutionCount++;
+        }
+
+        public async Task UpdateTask(TaskTable task, ReturnMessage message)
+        {
+            taskTable = task;
+            ExecutionCount++;
+        }
+        public async Task<List<TaskTable>> GetTasks(ReturnMessage message)
+        {
+
+
+            ExecutionCount++;
+
+            List <TaskTable > tasks = new List <TaskTable> ();
+
+            return tasks;
+        }
+
+
+        public int ExecutionCount { get; private set; }
+        public TaskTable taskTable { get; private set; }
+
+
+    }
+}
